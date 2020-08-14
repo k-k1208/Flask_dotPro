6e83a2ff7f8425b75ae6e035278b866c029ba2cd
Kei <dkk.1286@icloud.com>
2020-08-14 17:21:23 +0900

    done with flask4

--- flask4/app.py
+++ flask4/app.py
@@ -2,42 +2,9 @@
 # -*- coding: utf-8 -*-# -*- coding: utf-8 -*-
 # 日本語でコメント書くため,utf-8の文字コードと宣言
 from flask import Flask, render_template, request, redirect, url_for
-import json
 import sqlite3
 app = Flask(__name__)
 
-
-def prime_judge(x):
-    judge = True
-    for i in range(2, x):
-        if x % i == 0:
-            judge = False
-        else:
-            pass
-    if judge == True:
-        judge="素数"
-    elif judge == False:
-        judge="素数じゃない"
-    else:
-        judge="予想外のことが起きているで"
-    return judge
-
-
-'''
-#flask3ではjson.file形式だったので
-
-def get_profile():
-    # JSONファイルの読み込み
-    with open("data/profile.json", 'r') as f:
-        prof_dict = json.load(f)
-    user_list = list(prof_dict.keys())
-    return prof_dict, user_list
-
-# エンコード（JSON形式に変換）→json.dumps（）メソッド
-# デコード（JSON形式から自分のプログラムで使用できる形式に変換) →json.loads（）メソッド
-
-
-'''
 
 #  flask3 :json file形式
 #  flask4 :DBfile形式
@@ -61,13 +28,18 @@ cur.execute("insert into people values (?, ?)", (who, age))
 # And this is the named style:
 cur.execute("select * from people where name_last=:who and age=:age", {"who": who, "age": age})
 '''
-def update_profile(prof):
+
+
+def update_profile(prof_dict):
     conn = sqlite3.connect('profile.sqlite3')
     c = conn.cursor()
-    c.execute('update persons set name=?,age=?,sex=? where id=?',
-              (prof['name'], prof['age'], prof['sex'], prof['id']))  # qmark style
+    c.execute('UPDATE persons SET NAME=?,age=?,sex=? WHERE id=?',
+              (prof_dict['name'], prof_dict['age'], prof_dict['sex'], prof_dict['id']))  # qmark style
     conn.commit()
     conn.close()
+
+
+
 
 '''
 request.args: the key/value pairs in the URL query string
@@ -78,17 +50,38 @@ request.form['name']: use indexing if you know the key exists
 request.form.get('name'): use get if the key might not exist
 '''
 
+
 @app.route('/profile')
 def profile():
-
     prof_dict = get_profile()
     return render_template('profile.html', title='json', user=prof_dict)
+
 
 @app.route('/edit/<int:id>')
 def edit(id):
     prof_list = get_profile()
     prof_dict = prof_list[id-1]
     return render_template('edit.html', title='sql', user=prof_dict)
+
+# addする情報をformにinputしてpostするためのhtmlページに遷移させる部分
+@app.route('/add')
+def add():
+    return render_template('add.html', title='新規ユーザー登録')
+
+
+#  dotPro公式回答ではupdateのように　insert_profile関数を別に作っている。
+@app.route('/insert', methods=['POST'])
+def insert():
+    person_name = request.form['name']
+    person_age = request.form['age']
+    person_sex = request.form['sex']
+    conn = sqlite3.connect('profile.sqlite3')
+    c = conn.cursor()
+    c.execute('INSERT INTO persons (name, age, sex) VALUES (?, ?, ?)', (person_name, person_age, person_sex))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('profile'))
+
 
 @app.route('/update/<int:id>', methods=['POST'])
 def update(id):
@@ -98,9 +91,18 @@ def update(id):
     prof_dict['name'] = request.form['name']
     prof_dict['age'] = request.form['age']
     prof_dict['sex'] = request.form['sex']
-
     update_profile(prof_dict)
 
     return redirect(url_for('profile'))
+
+@app.route('/delete/<int:id>', methods=['GET'])
+def delete(id):
+    conn = sqlite3.connect('profile.sqlite3')
+    c = conn.cursor()
+    c.execute('DELETE FROM persons WHERE id=(?)', (id,))  # (id,)の部分気持ち悪いけど、こう書かないとダメらしい、、
+    conn.commit()
+    conn.close()
+    return redirect(url_for('profile'))
+
 if __name__ == "__main__":
     app.run(debug=True)
